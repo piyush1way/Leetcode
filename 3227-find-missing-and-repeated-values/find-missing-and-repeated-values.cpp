8e53ad2ba2cc880774eaaaa77db0cbc4711f2e6d
piyush1way <86835187+piyush1way@users.noreply.github.com>
2025-03-06 10:53:53 +0530

    Time: 0 ms (100.00%) | Memory: 25.4 MB (97.94%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -2,22 +2,26 @@ class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         int n = grid.size();
-        unordered_map<int, int> mpp;
-        vector<int> ans;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                mpp[grid[i][j]]++;
-                if (mpp[grid[i][j]] == 2) {
-                    ans.push_back(grid[i][j]);
+        vector<int>ans;
+        for(int i =0;i<n;i++){
+            for(int j =0;j<n;j++){
+                int curr = abs(grid[i][j]);
+                int row = (curr-1)/n;
+                int col = (curr-1)%n;
+                if(grid[row][col]<0){
+                    ans.push_back(abs(grid[i][j]));
+                }
+                else{
+                    grid[row][col]=grid[row][col]*-1;
                 }
             }
         }
-        for (int i = 1; i <= n * n; i++) {
-            if (mpp.find(i) != mpp.end()) {
-                continue;
-            } else {
-                ans.push_back(i);
-                break;
+        for(int i =0;i<n;i++){
+            for(int j =0;j<n;j++){
+                if(grid[i][j]>0){
+                    ans.push_back(i*n+j+1);
+                    break;
+                }
             }
         }
         return ans;
