ed7e101456331951634965b12032313e4ce89068
piyush1way <86835187+piyush1way@users.noreply.github.com>
2025-04-02 21:38:44 +0530

    Time: 4 ms (31.39%) | Memory: 8.3 MB (96.22%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -10,22 +10,17 @@ public:
             return word1;
         }
         int i = 0;
-        int j = 0;
         string ans = "";
-        while (i < n && j < m) {
-            ans.push_back(word1[i]);
-            ans.push_back(word2[j]);
+        while (i < n || i < m) {
+            if (i < n) {
+                ans.push_back(word1[i]);
+            }
+            if (i < m) {
+                ans.push_back(word2[i]);
+            }
             i++;
-            j++;
         }
-        while(i<n){
-            ans.push_back(word1[i]);
-            i++;
-        }
-        while(j<m){
-            ans.push_back(word2[j]);
-            j++;
-        }
+
         return ans;
     }
 };
