7cc90f308cfe2b605281af3fc8897eac0abf8ab2
piyush1way <86835187+piyush1way@users.noreply.github.com>
2025-05-21 13:49:28 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (91.40%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        int n = trips.size();
-        sort(trips.begin(), trips.end(),
-             [](const vector<int>& a, const vector<int>& b) {
-                 return a[1] < b[1];
-             });
+        sort(trips.begin(), trips.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];
+        });
+        // min-heap: pair<dropOffLocation, numPassengers>
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
 
-        for (int i = 0; i < n; i++) {
-            int currPassengers = trips[i][0];
-            int end = trips[i][2];
+        int currentPassengers = 0;
 
-            for (int j = 0; j < i; j++) {
-                // If a previous trip's drop-off is after current trip's start,
-                // the passengers are still in the car
-                if (trips[j][2] > trips[i][1]) {
-                    currPassengers += trips[j][0];
-                }
+        for (const auto& trip : trips) {
+            int num = trip[0];
+            int from = trip[1];
+            int to = trip[2];
+
+            while (!pq.empty() && pq.top().first <= from) {
+                currentPassengers -= pq.top().second;
+                pq.pop();
             }
+            currentPassengers += num;
+            if (currentPassengers > capacity) return false;
 
-            if (currPassengers > capacity)
-                return false;
+            pq.push({to, num});
         }
 
         return true;
